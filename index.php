f1c2ade11dfdd303a491171f75ed5c967154ed7c
ravi <31624180+Ravi0210@users.noreply.github.com>
2019-02-07 11:08:42 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,3 +5,33 @@
  * Date: 6-2-2019
  * Time: 10:50
  */
+<?php
+
+class klant{
+
+    public $auto;
+    public $naam;
+    public $email;
+
+public function  __constructor($auto,$naam,$email){
+    $this->auto=$auto;
+    $this->naam=$naam;
+    $this->email=$email;
+
+}}
+class auto{
+
+    public $model;
+    public $merk;
+    public $bouwjaar;
+    public $kenteken;
+
+    public function  __constructor($model , $merk , $bouwjaar , $kenteken){
+        $this->model=$model;
+        $this->merk=$merk;
+        $this->bouwjaar=$bouwjaar;
+        $this->kenteken=$kenteken;
+
+    }
+
+}
